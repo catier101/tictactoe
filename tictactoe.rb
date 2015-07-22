7919f8f99b95a56b94a862918dd5e19cb75e27c0
catier101 <catier101@gmail.com>
2015-07-22 20:06:24 +0000

    fixed player 2 and all board places viable

--- tictactoe.rb
+++ tictactoe.rb
@@ -30,28 +30,54 @@ class Board
     print @botmid
     print @botleft
   end
-  def update
-    if @move1.upcase == "A1"
+  def update1
+    if @move1 == "A1"
       @topright = "X"
-    elsif @move1.upcase == "A2"
+    elsif @move1 == "A2"
       @topmid = "X"
-    elsif @move1.upcase == "A3"
+    elsif @move1 == "A3"
       @topleft = "X"
-    elsif @move2.upcase == "A1"
-      @topright = "O"
+    elsif @move1 == "B1"
+      @midright = "X"
+    elsif @move1 == "B2"
+      @midmid = "X"
+    elsif @move1 == "B3"
+      @midleft == "X"
+     elsif @move1 == "C1"
+      @botright = "X"
+    elsif @move1 == "C2"
+      @botmid = "X"
+    elsif @move1 == "C3"
+      @botleft = "X"
     end
   end
   def update2
     if @move2.upcase == "A1"
+      @topright = "O"
+      elsif @move2 == "A2"
+      @topmid = "O"
+    elsif @move2 == "A3"
       @topleft = "O"
+    elsif @move2 == "B1"
+      @midright = "O"
+    elsif @move2 == "B2"
+      @midmid = "O"
+    elsif @move2 == "B3"
+      @midleft == "O"
+    elsif @move2 == "C1"
+      @botright = "O"
+    elsif @move2 == "C2"
+      @botmid = "O"
+    elsif @move3 == "C3"
+      @botleft = "O"
     end
   end
 end
 
 def game
   count = 0
+  new = Board.new
   until count > 9 do
-    new = Board.new
     puts "
   |A1|A2|A3|
   |B1|B2|B3|
@@ -59,11 +85,12 @@ def game
     if count.even? == true
       puts "Player One: make a move (choose from display above)"
       new.getplayer1
+      new.update1
     else
       puts "Player Two: make a move (choose from display above)"
       new.getplayer2
+      new.update2
     end
-    new.update
     new.putboard
     count = count + 1
     puts count
